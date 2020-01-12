f2118332e4927b2a6a694dbe459385e89e2fbb68
billyball1517 <54004311+billyball1517@users.noreply.github.com>
2020-01-12 12:22:58 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-/usr/sbin/snmpd -LS0-6d -f &
+/opt/splunk/bin/splunk cmd splunkd rest --noauth POST /services/authentication/users "name=splunk&password=splunkpass&roles=admin" &
 /opt/splunk/bin/splunk start --accept-license &
+
+/usr/sbin/snmpd -LS0-6d -f &
 
 exec gosu root "$@"
